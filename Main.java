c2d158903bbbe8d1f67b0cb148ee5b845368831a
dyiellcorpdy0 <blak.wolf09062006@gmail.com>
2024-02-05 22:10:34 +0300

    TEST3

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import static java.lang.Math.pow;
+import java.util.Scanner;
+import static java.lang.Math.pow;
 public class Main {
     public static void main(String[] args) {
         int a = 6;
@@ -14,7 +16,18 @@ public class Main {
             System.out.println("Ошибка, секретное число должно быть больше 0");
             return;
         }
-
+        int Xj= scanner.nextInt();
+        if(Xj<0) {
+            System.out.println("Ошибка, секретное число должно быть больше 0");
+            return;
         }
-
+        A = pow( a, Xi ) % p;
+        System.out.println("Публичное число первого пользователя " + A);
+        B = pow( a, Xj ) % p;
+        System.out.println("Публичное число второго пользователя " + B);
+        Zij = pow(B, Xi) % p;
+        System.out.println("Общий секретный ключ первого пользователя " + Zij);
+        Zji = pow(A, Xj) % p;
+        System.out.println("Общий секретный ключ второго пользователя " + Zji);
+    }
 }
